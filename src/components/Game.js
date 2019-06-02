a7e04241e17af5daf45aade74399ef9bb8f80568
antonellam <antonella.michelli@gmail.com>
2019-06-02 18:28:18 +0200

    Get comments from api and paint them on screen

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,48 +1,47 @@
-import React from 'react';
-import Comments from './Comments';
-import Feedback from './Feedback';
-import ShareIdea from './ShareIdea';
+import React from "react";
+import Comments from "./Comments";
+import Feedback from "./Feedback";
+import ShareIdea from "./ShareIdea";
 
 class Game extends React.Component {
-	constructor(props) {
-	  super(props);
-	  this.state = {
-		game: {}
-	  };
-	}
-  
-	componentWillMount() {
-	  this.setState({ game: this.props.game });
-	}
-	handleButtonClick = () => {
-		const apiUrl = '';
-	  fetch(
-		`${apiUrl}?body=${this.props.value}&game_id=${this.state.game.id}&parent_id=5858`,
-		{
-		  method: "POST"
-		}
-	  ).catch(error => console.log(error));
-	};
-	filterComments() {
-	  console.log(this.state.game.comments)
-	  const filteredComments = this.state.game.comments.filter(comment => comment.body ? comment.body.length > 10 : false);
-	  return filteredComments;
-	}
-  
-	render() {
-	  return (
-		<div>
-		  <Comments comments={this.filterComments()}/>
-		  <Feedback />
-		  <ShareIdea
-			value={this.props.value}
-			handleTextArea={this.props.handleTextArea}
-			handleButtonClick={this.handleButtonClick}
-		  />
-		</div>
-	  );
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      game: {}
+    };
   }
-  
-  export default Game;
-  
+
+  componentWillMount() {
+    this.setState({ game: this.props.game });
+  }
+  handleButtonClick = () => {
+	  const apiUrl = '';
+    fetch(
+      `${apiUrl}?body=${this.props.value}&game_id=${this.state.game.id}&parent_id=5858`,
+      {
+        method: "POST"
+      }
+    ).catch(error => console.log(error));
+  };
+  filterComments() {
+    console.log(this.state.game.comments)
+    const filteredComments = this.state.game.comments.filter(comment => comment.body ? comment.body.length > 10 : false);
+    return filteredComments;
+  }
+
+  render() {
+    return (
+      <div>
+        <Comments comments={this.filterComments()}/>
+        <Feedback />
+        <ShareIdea
+          value={this.props.value}
+          handleTextArea={this.props.handleTextArea}
+          handleButtonClick={this.handleButtonClick}
+        />
+      </div>
+    );
+  }
+}
+
+export default Game;
