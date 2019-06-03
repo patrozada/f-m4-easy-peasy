011a1b78c8be50cae4fb7591ee024e738d3aa282
patrozada <patriciarozada@gmail.com>
2019-06-03 17:06:10 +0200

    Add preliminar styles

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import { ENDPOINT_FEEDBACK } from './../services/fetchPostFeedback';
+import React from "react";
+import { ENDPOINT_FEEDBACK } from "./../services/fetchPostFeedback";
+import sad from "../images/sad.svg";
+import neutral from "../images/neutral.svg";
+import happy from "../images/happy.svg";
+import wow from "../images/wow.svg";
 
 class Feedback extends React.Component {
 	handleFeedback = e => {
 		const { value } = e.target;
 
 		fetch(ENDPOINT_FEEDBACK, {
-			method: 'POST',
+			method: "POST",
 			body: JSON.stringify({
 				parent_id: 6281,
 				pod_id: 609,
@@ -15,25 +19,44 @@ class Feedback extends React.Component {
 			})
 		})
 			.then(res => res.json())
-			.catch(error => console.error('Error:', error))
-			.then(response => console.log('Success:', response));
+			.catch(error => console.error("Error:", error))
+			.then(response => console.log("Success:", response));
 	};
 
 	render() {
 		return (
-			<React.Fragment>
-				<form onChange={this.handleFeedback} action="POST">
-					<h1>HOW DID IT GO?</h1>
-					<label htmlFor="1" />
-					<input type="radio" value={1} name="feedback" id="1" />
-					<label htmlFor="2" />
-					<input type="radio" value={2} name="feedback" id="2" />
-					<label htmlFor="3" />
-					<input type="radio" value={3} name="feedback" id="3" />
-					<label htmlFor="4" />
-					<input type="radio" value={4} name="feedback" id="4" />
+			<section className='feedback--container'>
+				<p className='feedback--title'>How did it go?</p>
+				<form
+					className='feedback--input__container'
+					onChange={this.handleFeedback}
+					action='POST'>
+					<label htmlFor='1'>
+						<input type='radio' value={1} name='feedback' id='1' />
+						<div className='feedback--icon__container'>
+							<img className="feedback--icon" src={sad} alt='sad face' />
+						</div>
+					</label>
+					<label htmlFor='2'>
+						<input type='radio' value={2} name='feedback' id='2' />
+						<div className='feedback--icon__container'>
+							<img className="feedback--icon" src={neutral} alt='neutral face' />
+						</div>
+					</label>
+					<label htmlFor='3'>
+						<input type='radio' value={3} name='feedback' id='3' />
+						<div className='feedback--icon__container'>
+							<img className="feedback--icon" src={happy} alt='happy face' />
+						</div>
+					</label>
+					<label htmlFor='4'>
+						<input type='radio' value={4} name='feedback' id='4' />
+						<div className='feedback--icon__container'>
+							<img className="feedback--icon"src={wow} alt='wow face' />
+						</div>
+					</label>
 				</form>
-			</React.Fragment>
+			</section>
 		);
 	}
 }
