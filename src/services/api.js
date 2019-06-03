b12158f2b39b3b84826331b66206a9ec29c6b253
ioanadamo <daniela932910@gmail.com>
2019-06-03 10:29:41 +0200

    update feedback

--- src/services/api.js
+++ src/services/api.js
@@ -1,3 +1,5 @@
 export const API_BASE = process.env.REACT_APP_API_BASE_DOMAIN;
 
 export const API_COMMENTS = process.env.REACT_APP_API_ENDPOINT_COMMENTS;
+
+export const API_FEEDBACK = process.env.REACT_APP_API_ENDPOINT_FEEDBACK;
--- src/services/fetchPostComments.js
+++ src/services/fetchPostComments.js
@@ -2,6 +2,4 @@ import * as api from './api';
 
 const ENDPOINT = api.API_COMMENTS;
 
-// const fetchPostComments = () => fetch(ENDPOINT);
-
 export { ENDPOINT };
