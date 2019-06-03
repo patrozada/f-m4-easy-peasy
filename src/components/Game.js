1b075b9546819a5b30ce3b5be556d5ef75e75609
ioanadamo <daniela932910@gmail.com>
2019-06-03 13:45:19 +0200

    add header elements

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,43 +1,43 @@
-import React from "react";
-import Header from "./Header";
-import Carousel from "./Carousel"
-import Comments from "./Comments";
-import Feedback from "./Feedback";
-import ShareIdea from "./ShareIdea";
-import CommentSection from "./CommentSection";
-import MoreGames from "./MoreGames";
-import { ENDPOINT } from "./../services/fetchPostComments";
+import React from 'react';
+import Header from './Header';
+import Carousel from './Carousel';
+import Comments from './Comments';
+import Feedback from './Feedback';
+import ShareIdea from './ShareIdea';
+import CommentSection from './CommentSection';
+import MoreGames from './MoreGames';
+import { ENDPOINT } from './../services/fetchPostComments';
 
 class Game extends React.Component {
 	handleButtonClick = () => {
-		fetch(
-			`${ENDPOINT}?body=${this.props.value}&game_id=${21}&parent_id=6281`,
-			{
-				method: 'POST'
-			}
-		).catch(error => console.log(error));
+		fetch(`${ENDPOINT}?body=${this.props.value}&game_id=${21}&parent_id=6281`, {
+			method: 'POST'
+		}).catch(error => console.log(error));
 	};
 
 	render() {
-		const {games} = this.props;
+		const { games } = this.props;
 		return (
 			<div>
 				{games
 					.filter(game => game.id === 21)
 					.map(game => {
-						return <div>
-            <Header />
-				    <Carousel />
-						<Comments game={game} />
-						<Feedback game={game} />
-						<ShareIdea
-							value={this.props.value}
-							handleTextArea={this.props.handleTextArea}
-							handleButtonClick={this.handleButtonClick}
-						/>
-            <CommentSection />
-				    <MoreGames />
-						</div>})}
+						return (
+							<div>
+								<Header game={game} />
+								<Carousel />
+								<Comments game={game} />
+								<Feedback game={game} />
+								<ShareIdea
+									value={this.props.value}
+									handleTextArea={this.props.handleTextArea}
+									handleButtonClick={this.handleButtonClick}
+								/>
+								<CommentSection />
+								<MoreGames />
+							</div>
+						);
+					})}
 			</div>
 		);
 	}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,33 @@
 import React from 'react';
 
 class Header extends React.Component {
-  render(){
-    return(
-      <p>This is Game Header</p>
-    )
-  }
+	render() {
+		const { game } = this.props;
+		return (
+			<React.Fragment>
+				<section className="header__nav">
+					<nav>
+						<ul>
+							<li>
+								<a href="">Games</a> <a href="">Progress</a> 
+								<a href="">Awards</a>             
+							</li>
+						</ul>
+					</nav>
+				</section>
+				<section className="header__titles">
+					<h1>{game.name}</h1>  
+					<p>{game.description}</p> 
+				</section>
+				<section>
+					<div
+						className="header__image-game"
+						style={{ backgroundImage: `url(${game.image_url})` }}
+					/>
+					<img />
+				</section>
+			</React.Fragment>
+		);
+	}
 }
 export default Header;
