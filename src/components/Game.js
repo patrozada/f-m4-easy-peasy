e88b5c78ca0b2bb7783020c4a0209c1653f12050
ioanadamo <daniela932910@gmail.com>
2019-06-03 17:52:56 +0200

    Revert "Merge pull request #13 from Adalab/header"
    This reverts commit f735d67b519ca5411d047a7869bfa1b811ea23d8, reversing
    changes made to 3c6f9c84d04876be981c6c26def99160567e02c8.

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,44 +1,34 @@
+import React from "react";
+import Header from "./Header";
+import Button from "./Button";
+import Carousel from "./Carousel"
+import Comments from "./Comments";
+import Feedback from "./Feedback";
+import MoreGames from "./MoreGames";
+import { Link } from 'react-router-dom';
 
-import React from 'react';
-import Header from './Header';
-import Carousel from './Carousel';
-import Comments from './Comments';
-import Feedback from './Feedback';
-import ShareIdea from './ShareIdea';
-import CommentSection from './CommentSection';
-import MoreGames from './MoreGames';
-import { ENDPOINT } from './../services/fetchPostComments';
 
 class Game extends React.Component {
-	handleButtonClick = () => {
-		fetch(`${ENDPOINT}?body=${this.props.value}&game_id=${21}&parent_id=6281`, {
-			method: 'POST'
-		}).catch(error => console.log(error));
-	};
+	
+
 	render() {
-		const { games } = this.props;
+		const {games} = this.props;
 		return (
 			<React.Fragment>
 				{games
 					.filter(game => game.id === 21)
 					.map(game => {
-						return (
-							<div>
-								<Header game={game} />
-								<Carousel />
-								<Comments game={game} />
-								<Feedback game={game} />
-								<ShareIdea
-									value={this.props.value}
-									handleTextArea={this.props.handleTextArea}
-									handleButtonClick={this.handleButtonClick}
-								/>
-								<CommentSection />
-								<MoreGames />
-							</div>
-						);
-					})}
-			</div>
+						return <div>
+            <Header />
+						<Link to={`/game/${game.id}/comment`}>
+							<Button />
+						</Link>
+						<Carousel game={game}/>
+						<Comments game={game} />
+						<Feedback game={game} />
+				    <MoreGames />
+						</div>})}
+			</React.Fragment>
 		);
 	}
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,65 +1,10 @@
 import React from 'react';
-import imgPlay from '../images/play-white@1x.png';
-import imgSettings from '../images/settings-default.png';
-import './header.scss';
 
 class Header extends React.Component {
-	render() {
-		const { game } = this.props;
-		return (
-			<header>
-				<section className="header__nav">
-					<nav>
-						<ul className="header__nav-list">
-							<li>
-								<a className="header__nav-list-link" href="#">
-									Games
-								</a>
-								 
-							</li>
-							<li>
-								<a className="header__nav-list-link" href="#">
-									Progress
-								</a>
-								 
-							</li>
-							<li>
-								<a className="header__nav-list-link" href="#">
-									Awards
-								</a>
-								 
-							</li>
-							<li>
-								<img
-									className="header__nav-settingsImage-elem"
-									src={imgSettings}
-									alt="image-settings"
-								/>
-							</li>
-							      
-						</ul>
-					</nav>
-				</section>
-				<section>
-					<h1 className="header__title">{game.name}</h1>  
-					<p className="header__subtitle">{game.description}</p> 
-				</section>
-				<section className="header__image-section">
-					<div
-						className="header__image-game"
-						style={{ backgroundImage: `url(${game.image_url})` }}
-					/>
-					<div className="header__image-play-container">
-						{' '}
-						<img
-							className="header__image-play"
-							src={imgPlay}
-							alt="image-play"
-						/>
-					</div>
-				</section>
-			</header>
-		);
-	}
+  render(){
+    return(
+      <p>This is Game Header</p>
+    )
+  }
 }
 export default Header;
