4928a6b5431a6d2e1365d5a06b53b237891071f1
anniavdc <anva1189@gmail.com>
2019-06-04 12:27:08 +0200

    Add first styles

--- src/components/ShareIdea.js
+++ src/components/ShareIdea.js
@@ -5,7 +5,7 @@ class ShareIdea extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <form method="POST">
+        <form method="POST" className="shareIdea--container">
           <label htmlFor="comment">Share ideas or observations</label>
           <p>Let others in your pod how your play went!</p>
           <textarea
@@ -17,7 +17,7 @@ class ShareIdea extends React.Component {
             value={this.props.value}
             className="shareIdea--input"
           />
-          <Button type="button" onClick={this.props.handleButtonClick} variant="success" style={{backgroundColor:`#7db238`}}>Add your comment</Button>
+          <Button type="button" onClick={this.props.handleButtonClick} variant="success" style={{backgroundColor:`#7db238`}} className="shareIdea--button">Add your comment</Button>
         </form>
       </React.Fragment>
     );
