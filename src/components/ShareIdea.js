b62777a3e805d19567ce8c881a3295f7c0f80b57
anniavdc <anva1189@gmail.com>
2019-06-04 13:07:25 +0200

    Add more styles

--- src/components/ShareIdea.js
+++ src/components/ShareIdea.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Button from 'react-bootstrap/Button';
+import ShareIdeaImage from './../images/103-onboarding@1x.png'
 
 class ShareIdea extends React.Component {
   render() {
     return (
-      <React.Fragment>
-        <form method="POST" className="shareIdea--container">
+      <div className="shareIdea--container">
+        <img src={ShareIdeaImage} alt="happy beens talking" className="shareIdea--image"/>
+        <form method="POST" className="shareIdea--input__container">
           <label htmlFor="comment">Share ideas or observations</label>
           <p>Let others in your pod how your play went!</p>
           <textarea
@@ -17,9 +19,18 @@ class ShareIdea extends React.Component {
             value={this.props.value}
             className="shareIdea--input"
           />
-          <Button type="button" onClick={this.props.handleButtonClick} variant="success" style={{backgroundColor:`#7db238`}} className="shareIdea--button">Add your comment</Button>
+          <Button 
+          type="button" 
+          onClick={this.props.handleButtonClick} variant="success" 
+          style={{
+            backgroundColor:'#7db238',
+            fontFamily:"'Lato', sans-serif",
+            textTransform:'uppercase',
+            fontSize:'12px',
+            border:'1px solid #7db238'
+            }}className="shareIdea--button">Add your comment</Button>
         </form>
-      </React.Fragment>
+      </div>
     );
   }
 }
