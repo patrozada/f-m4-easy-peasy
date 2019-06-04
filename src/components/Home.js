24bf3f41ec75b3653fd1e919faa6a5e9aad5b813
antonellam <antonella.michelli@gmail.com>
2019-06-04 16:54:42 +0200

    Create initial card and other games' cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,41 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import { Link } from 'react-router-dom';
 
-const Home = (props) => {
-  return (
-    props.games.map(game => 
-      <Link to={`/game/${game.id}`}><button>Go to {game.name}</button></Link>
-    )
-  );
+class Home extends React.Component {
+  render() {
+    const initialGame = this.props.games.find(game => parseInt(game.id) === 21);
+    console.log(initialGame);
+    return initialGame === undefined ? <p>Loading...</p> :(
+      <React.fragment>
+        <Card>
+          <Card.Header>Hi parent! It's week 1 of 8</Card.Header>
+          <Card.Body>
+            <Card.Title>{initialGame.name}</Card.Title>
+            <Card.Text>
+              {initialGame.description}
+            </Card.Text>
+            <Button variant="primary">Play new game</Button>
+          </Card.Body>
+        </Card>
+        <div>
+          {this.props.games.map(game => 
+            <Card>
+              <Card.Img variant="top" src={game.image_url} />
+              <Card.Body>
+                <Card.Title>{game.name}</Card.Title>
+                <Card.Text>
+                  {game.description}
+                </Card.Text>
+                <Link to={`/game/${game.id}`}><Button variant="primary">Go to {game.name}</Button></Link>
+              </Card.Body>
+            </Card>
+          )}
+        </div>
+      </React.fragment>
+    );
+  }
 }
- 
+
 export default Home;
