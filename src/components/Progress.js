0f0489a2d5d996a7fbb99a4386deda3a71709930
ioanadamo <daniela932910@gmail.com>
2019-06-04 11:36:48 +0200

    add images

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import ProgressBar from 'react-bootstrap/ProgressBar';
+import PhisicalIcon from '../images/physical-1@1x.png';
+import CommunicationIcon from '../images/communication-1@1x.png';
+import WorldIcon from '../images/world-1@1x.png';
 
 class Progress extends React.Component {
 	constructor(props) {
@@ -12,20 +15,23 @@ class Progress extends React.Component {
 				<p>How the game helps your child’s progress</p>
 				<div>
 					<div>
-						<p>Phisical</p>
+						<p>Physical</p>
 						<p>Level 1</p>
 					</div>
 					<ProgressBar variant="success" now={40} />
+					<img src={PhisicalIcon} alt="Phisical-Icon" />
 					<div>
 						<p>Communication</p>
 						<p>Level 1</p>
 					</div>
 					<ProgressBar variant="info" now={20} />
+					<img src={CommunicationIcon} alt="Communication-Icon" />
 					<div>
 						<p>World</p>
 						<p>Level 1</p>
 					</div>
 					<ProgressBar variant="warning" now={60} />
+					<img src={WorldIcon} alt="World-Icon" />
 				</div>
 			</section>
 		);
