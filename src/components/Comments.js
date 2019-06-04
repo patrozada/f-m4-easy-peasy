d2165352de59cd26f522ab581056870d27479cec
antonellam <antonella.michelli@gmail.com>
2019-06-04 11:15:46 +0200

    Retrieve three-last comments from array

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -3,15 +3,13 @@ import React from "react";
 class Comments extends React.Component {
   render() {
     const { game } = this.props;
-
+    const gameCommentsWithBody = game.comments.filter(comment => comment.body ? comment.body.length > 1 : false);
+    const threeLastComments = gameCommentsWithBody.slice(gameCommentsWithBody.length - 3)
     return (
       <div>
-		{game.comments
-		.filter(comment =>
-          comment.body ? comment.body.length > 1 : false
-        ).map(comment =>
-			<p>{comment.body}</p>
-		)}
+        {threeLastComments.map(comment => 
+          <p>{comment.body}</p>
+        )}
       </div>
     );
   }
